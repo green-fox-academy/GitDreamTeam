aaca5a63e628e9a3f00ac2028c43b391e636f3a9
Andras <palfi.andras89@gmail.com>
2018-07-31 10:58:20 +0200

    chore(Greet)

---
+++ Greet/Greet/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Greet());
+            Console.ReadLine();
+        }
+
+        private static string Greet()
+        {
+            return "Hello Andrew!";
+        }
+    }
+}
